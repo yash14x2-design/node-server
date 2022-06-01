a1fbb92ca74a03fcc83fb7e6e24a32deabf2a3d1
Yash Joshi <Yash.Joshi@teknopoint.local>
2022-06-01 13:04:25 +0530

    removed the server file

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer(function (req, res) {
-  fs.readFile(`${__dirname}/data.txt`, (err, data) => {
-    res.end(data);
-  });
-});
-server.listen(3000);
