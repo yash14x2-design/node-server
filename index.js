183980a28c0337f44ca1ba83cd7b5edebd059e80
Yash Joshi <Yash.Joshi@teknopoint.local>
2022-06-01 17:20:55 +0530

    final done using express

--- index.js
+++ index.js
@@ -1,99 +1,26 @@
-// const http = require("http");
-var http = require('http');
-const fs = require("fs");
+
+var compression = require('compression')
+const express = require("express");
+
+const app = express();
+app.use(compression())
 
 
-const zlib = require('zlib');
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
 
 
-// const server = http.createServer({
-//   // key: fs.readFileSync("./private.pem"),
-//   // cert: fs.readFileSync("./cert.pem"),
-// });
 
-// server.on("error", function (err) {
-//   console.log(err);
-// });
+// serve your css as static
+app.use(express.static(__dirname));
 
-// server.on("stream", function (stream, headers) {
-//   stream.respond({
-//     "content-type": "text/html",
-//     'Content-Encoding': 'gzip',
-//     status: 200,
-//   });
-
-//   if (headers[":path"] == "/student") {
-    
-    
-//       fs.readFile("index.html",function(error, data){
-//       if (error) {
-//         resp.writeHead(404);
-//         resp.write('Contents you are looking for-not found');
-//         resp.end();
-//       }  else {
-
-        
-//     zlib.gzip(data.toString(), function (_, result) {  // The callback will give you the 
-//       stream.end(result);                     // result, so just send it.
-//     });
-        
-        
-//       }
-//     });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname +  "/index.html");
+});
 
 
 
 
 
-//     // zlib.gzip(text, function (_, result) {  // The callback will give you the 
-//     //   stream.end(result);                     // result, so just send it.
-//     // });
-    
-    
-    
-    
-//   } else {
-//     stream.end(
-//       '<h1>Hello world parammm</h1>'
-//     );
-//   }
-// });
-// var port = process.env.PORT || 8080;
 
-// server.listen(port, function () {
-//     console.log("http://localhost:8080", port);
-//   });
-
-var port = process.env.PORT || 8080;
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain' , "content-type": "text/html",
-    'Content-Encoding': 'gzip',
-    status: 200,});
-    if(req.url == "/student"){
-
-      fs.readFile("index.html",function(error, data){
-        if (error) {
-          resp.writeHead(404);
-          resp.write('Contents you are looking for-not found');
-          resp.end();
-        }
-
-        else{
-          
-   zlib.gzip(data, function (_, result) {  // The callback will give you the 
-    res.end(result);                     // result, so just send it.
-  });
-
-        }
-      })
-     
-    }
-    else{
-      res.end('Hello World!');
-
-    }
-   
-}).listen(port);
-
-
-  
