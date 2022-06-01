08e606f3d383da91905dcc3bf8fa902fdba88b89
Yash Joshi <Yash.Joshi@teknopoint.local>
2022-06-01 13:41:40 +0530

    useing http

--- index.js
+++ index.js
@@ -1,67 +1,73 @@
-const http2 = require("node:http2");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
 
 
-const zlib = require('zlib');
+// const zlib = require('zlib');
 
 
-const server = http2.createSecureServer({
-  key: fs.readFileSync("./private.pem"),
-  cert: fs.readFileSync("./cert.pem"),
-});
+// const server = http.createServer({
+//   // key: fs.readFileSync("./private.pem"),
+//   // cert: fs.readFileSync("./cert.pem"),
+// });
 
-server.on("error", function (err) {
-  console.log(err);
-});
+// server.on("error", function (err) {
+//   console.log(err);
+// });
 
-server.on("stream", function (stream, headers) {
-  stream.respond({
-    "content-type": "text/html",
-    'Content-Encoding': 'gzip',
-    status: 200,
-  });
+// server.on("stream", function (stream, headers) {
+//   stream.respond({
+//     "content-type": "text/html",
+//     'Content-Encoding': 'gzip',
+//     status: 200,
+//   });
 
-  if (headers[":path"] == "/student") {
+//   if (headers[":path"] == "/student") {
     
     
-      fs.readFile("index.html",function(error, data){
-      if (error) {
-        resp.writeHead(404);
-        resp.write('Contents you are looking for-not found');
-        resp.end();
-      }  else {
+//       fs.readFile("index.html",function(error, data){
+//       if (error) {
+//         resp.writeHead(404);
+//         resp.write('Contents you are looking for-not found');
+//         resp.end();
+//       }  else {
 
         
-    zlib.gzip(data.toString(), function (_, result) {  // The callback will give you the 
-      stream.end(result);                     // result, so just send it.
-    });
+//     zlib.gzip(data.toString(), function (_, result) {  // The callback will give you the 
+//       stream.end(result);                     // result, so just send it.
+//     });
         
         
-      }
-    });
+//       }
+//     });
 
 
 
 
 
-    // zlib.gzip(text, function (_, result) {  // The callback will give you the 
-    //   stream.end(result);                     // result, so just send it.
-    // });
+//     // zlib.gzip(text, function (_, result) {  // The callback will give you the 
+//     //   stream.end(result);                     // result, so just send it.
+//     // });
     
     
     
     
-  } else {
-    stream.end(
-      '<h1>Hello world parammm</h1>'
-    );
-  }
-});
+//   } else {
+//     stream.end(
+//       '<h1>Hello world parammm</h1>'
+//     );
+//   }
+// });
+// var port = process.env.PORT || 8080;
+
+// server.listen(port, function () {
+//     console.log("http://localhost:8080", port);
+//   });
+var http = require('http');
 var port = process.env.PORT || 8080;
-
-server.listen(port, function () {
-    console.log("https://localhost:8080", port);
-  });
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(port);
 
 
   
