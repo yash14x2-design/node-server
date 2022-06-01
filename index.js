d2c2c4ae1785497dcf79ac7c9e0e6151deb42080
Yash Joshi <Yash.Joshi@teknopoint.local>
2022-06-01 15:27:17 +0530

    applied gzip

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // const http = require("http");
-// const fs = require("fs");
+var http = require('http');
+const fs = require("fs");
 
 
-// const zlib = require('zlib');
+const zlib = require('zlib');
 
 
 // const server = http.createServer({
@@ -62,12 +63,30 @@
 // server.listen(port, function () {
 //     console.log("http://localhost:8080", port);
 //   });
-var http = require('http');
+
 var port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/plain' , "content-type": "text/html",
+    'Content-Encoding': 'gzip',
+    status: 200,});
     if(req.url == "/student"){
-      res.end('Hello student!');
+
+      fs.readFile("index.html",function(error, data){
+        if (error) {
+          resp.writeHead(404);
+          resp.write('Contents you are looking for-not found');
+          resp.end();
+        }
+
+        else{
+          
+   zlib.gzip(data, function (_, result) {  // The callback will give you the 
+    res.end(result);                     // result, so just send it.
+  });
+
+        }
+      })
+     
     }
     else{
       res.end('Hello World!');
